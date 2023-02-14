4c580bb068676e810c8fb6fb335594f8f8b531c0
f0reachARR <f0reach@f0reach.me>
2023-02-14 23:19:04 +0900

    :card_file_box: Add getUser to user repository

--- src/repositories/users/repository.ts
+++ src/repositories/users/repository.ts
@@ -2,5 +2,6 @@ import { UncommitedUser, User } from '../../models/users';
 
 export interface UserRepository {
   registerUser(): Promise<User>;
+  getUser(id: string): Promise<User>;
   commitUser(user: UncommitedUser): Promise<User>;
 }
